44d9cbb9e634913b0cd4a769a6cddb2456f81ebb
Paul Johnson <paul_johnson1@next.co.uk>
2022-03-18 22:55:54 +0000

    Add project files.

---
+++ mauimap/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace mauimap;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		Task.Run(async () => await LaunchMap());
+	}
+
+	async Task LaunchMap()
+    {
+		var location = new Location(28.3772, 81.5707);
+		var options = new MapLaunchOptions { Name = "Disney World" };
+		//Launching Maps
+		await Map.OpenAsync(location, options);
+	}
+}
+
---
+++ mauimap/Platforms/Android/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+
+namespace mauimap;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+public class MainActivity : MauiAppCompatActivity
+{
+    const int RequestLocationId = 0;
+
+    readonly string[] LocationPermissions =
+    {
+        Manifest.Permission.AccessCoarseLocation,
+        Manifest.Permission.AccessFineLocation
+    };
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        if ((int)Build.VERSION.SdkInt >= 23)
+        {
+            if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+            {
+                RequestPermissions(LocationPermissions, RequestLocationId);
+            }
+            else
+            {
+                // Permissions already granted - display a message.
+            }
+        }
+    }
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+    {
+        if (requestCode == RequestLocationId)
+        {
+            if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted))
+            {
+            }
+        // Permissions granted - display a message.
+            else
+            { }
+            // Permissions denied - display a message.
+        }
+        else
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
